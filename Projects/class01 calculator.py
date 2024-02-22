412cd66c1cb79ef0cb1d1389915d28f30f8f6f44
Syedabdullahzaidi473 <144045196+Syedabdullahzaidi473@users.noreply.github.com>
2024-02-22 22:31:46 +0500

    Add files via upload
    html , css , javascript , python , typescript...

---
+++ Projects/class01 calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+# Addition
+print(f"{num1} + {num2} =", num1 + num2)
+
+# Subtraction
+print(f"{num1} - {num2} =", num1 - num2)
+
+# Multiplication
+print(f"{num1} * {num2} =", num1 * num2)
+
+# Division
+print(f"{num1} / {num2} =", num1 / num2)
